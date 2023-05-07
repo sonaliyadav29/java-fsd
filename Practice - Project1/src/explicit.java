934917d1326340811347a470f3f99f2e5f233dae
Sonali Yadav <132663771+sonaliyadav29@users.noreply.github.com>
2023-05-07 21:47:46 +0530

    Add files via upload

---
+++ Practice - Project1/src/explicit.java
@@ -0,0 +1,16 @@
+
+public class explicit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+double d=298.99;
+
+long l= (long)d;
+
+int i = (int)l;
+ System.out.println("Before conversion: "+d);
+ System.out.println("After converstion into long type: "+l);
+	System.out.println("After conversion into int type: "+i);
+	}
+
+}
